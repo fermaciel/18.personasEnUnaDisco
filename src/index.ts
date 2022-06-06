7679320aa25847face0ff3f1d2a5663442868ec5
fermaciel <104040518+fermaciel@users.noreply.github.com>
2022-06-06 17:27:12 -0300

    personas en una disco

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,29 @@ aleatorio(menorValor,mayorValor), es decir aleatorio(19,40))
 • Cuántas personas mayores o igual a 21 años
 • Cuántas personas en total */
 
+function aleatorio(menorValor: number, mayorValor: number): number {
+  return Math.floor(Math.random() * (mayorValor - menorValor) + menorValor);
+}
+function edades(personas: number[], parametroDeComparacion: number) {
+  let mayores: number = 0;
+  for (let i = 0; i < personas.length; i++) {
+    if (personas[i] >= parametroDeComparacion) {
+      mayores = mayores + 1;
+    }
+  }
+  console.log(
+    `hay\n ${
+      personas.length - mayores
+    } personas menores a 21 años ,\n ${mayores} personas mayores a  21  años \n y un total de ${
+      personas.length
+    } personas.`
+  );
+}
+let totalDePersonas: number = 270;
+
+let arrayEdades: number[] = new Array(totalDePersonas);
+for (let i = 0; i < totalDePersonas; i++) {
+  arrayEdades[i] = aleatorio(19, 40);
+}
+console.log(arrayEdades.length);
+edades(arrayEdades, 21);
