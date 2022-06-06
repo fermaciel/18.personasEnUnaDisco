eabd804999f051f37278c8b0dc4f3fbf01fd8091
fermaciel <104040518+fermaciel@users.noreply.github.com>
2022-06-06 09:18:40 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+/* Ejercicios de Repaso
+Ejercicio – Personas en una Disco
+
+• Para tener control de la gente que hay en una disco el gerente quiere
+saber cuantas personas de diferentes edades han entrado.
+• No se han permitido la entrada a menores de 18 ni mayores de 40.
+Para la carga de los datos se usa la función aleatorio (use la función
+aleatorio(menorValor,mayorValor), es decir aleatorio(19,40))
+• Se sabe que la cantidad total de personas dentro del local es de 270
+• Se quiere saber:
+• Cuántas personas son menores de 21 años
+• Cuántas personas mayores o igual a 21 años
+• Cuántas personas en total */
+
